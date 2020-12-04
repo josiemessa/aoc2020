ff6f61fe2ab33107f7b363445e662ee8fac98d0d
josiemessa <josie@unity3d.com>
2020-12-04 21:20:53 +0000

    Steal Alex's better solution for doing day 4

--- go/day4/day4_test.go
+++ go/day4/day4_test.go
@@ -22,7 +22,7 @@ hcl:#cfa07d eyr:2025 pid:166559648
 iyr:2011 ecl:brn hgt:59in`
 
 func TestDay4_Part1(t *testing.T) {
-	require.Equal(t, 2, SolveP1(strings.NewReader(input)))
+	require.Equal(t, 2, Solve(strings.NewReader(input), false))
 }
 
 func TestDay4_Part2_AllInvalid(t *testing.T) {
@@ -39,7 +39,7 @@ ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
 hgt:59cm ecl:zzz
 eyr:2038 hcl:74454a iyr:2023
 pid:3556412378 byr:2007`
-	require.Equal(t, 0, SolveP2(strings.NewReader(input)))
+	require.Equal(t, 0, Solve(strings.NewReader(input), true))
 
 }
 
@@ -56,94 +56,6 @@ pid:545766238 ecl:hzl
 eyr:2022
 
 iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719`
-	require.Equal(t, 4, SolveP2(strings.NewReader(input)))
+	require.Equal(t, 4, Solve(strings.NewReader(input), true))
 
-}
-
-func TestPassport_Parse(t *testing.T) {
-	type args struct {
-		field string
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
-	}{
-		{
-			name:    "byr valid:   2002",
-			args:    args{field: "byr:2002"},
-			wantErr: false,
-		},
-		{
-			name:    "byr, invalid: 2003",
-			args:    args{field: "byr:2003"},
-			wantErr: true,
-		},
-		{
-			name:    "hgt, valid: 60in",
-			args:    args{field: "hgt:60in"},
-			wantErr: false,
-		},
-		{
-			name:    "hgt, valid: 190cm",
-			args:    args{field: "hgt:190cm"},
-			wantErr: false,
-		},
-		{
-			name:    "hgt, invalid: 190in",
-			args:    args{field: "hgt:190in"},
-			wantErr: true,
-		},
-		{
-			name:    "hgt, invalid: 190",
-			args:    args{field: "hgt:190"},
-			wantErr: true},
-
-		{
-			name:    "hcl, valid:   #123abc",
-			args:    args{field: "hcl:#123abc"},
-			wantErr: false,
-		},
-		{
-			name:    "hcl, invalid: #123abz",
-			args:    args{field: "hcl:#123abz"},
-			wantErr: true,
-		},
-		{
-			name:    "hcl, invalid: 123abc",
-			args:    args{field: "hcl:123abc"},
-			wantErr: true},
-
-		{
-			name:    "ecl, valid: brn",
-			args:    args{field: "ecl:brn"},
-			wantErr: false,
-		},
-		{
-			name:    "ecl, invalid: wat",
-			args:    args{field: "ecl:wat"},
-			wantErr: true,
-		},
-		{
-			name:    "pid, valid: 000000001",
-			args:    args{field: "pid:000000001"},
-			wantErr: false,
-		},
-		{
-			name:    "pid, invalid: 0123456789}",
-			args:    args{field: "pid:0123456789}"},
-			wantErr: true,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			p := &Passport{}
-			err := p.Parse(tt.args.field)
-			if tt.wantErr {
-				require.Error(t, err)
-			} else {
-				require.NoError(t, err)
-			}
-		})
-	}
 }
